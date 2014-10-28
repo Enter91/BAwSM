f3dd2a5fb01173eb140b0437691238be5c4e49c5
dklimaszewski <dklimaszewski13@gmail.com>
2014-10-28 13:52:18 +0100

    no message

--- iCarTools/iCarTools/ViewController.m
+++ iCarTools/iCarTools/ViewController.m
@@ -50,6 +50,7 @@
 - (IBAction)statsOpenAction:(id)sender {
     NSURL *url = [NSURL URLWithString:@"http://maps.apple.com/maps?q=Szczecin"];
     [[UIApplication sharedApplication] openURL:url];
+    
 }
 
 - (IBAction)settingsOpenAction:(id)sender {

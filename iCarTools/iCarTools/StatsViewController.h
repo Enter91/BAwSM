93bb4870b6dbca24616ebb65e8a43fad7b6fe437
Damian Klimaszewski <dklimaszewski13@gmail.com>
2014-11-12 18:24:03 +0100

    StatsViewController and new xib.

---
+++ iCarTools/iCarTools/StatsViewController.h
@@ -0,0 +1,13 @@
+//
+//  StatsViewController.h
+//  iCarTools
+//
+//  Created by Damian Klimaszewski on 12.11.2014.
+//  Copyright (c) 2014 Michał Czwarnowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StatsViewController : UIViewController
+
+@end
---
+++ iCarTools/iCarTools/StatsViewController.m
@@ -0,0 +1,37 @@
+//
+//  StatsViewController.m
+//  iCarTools
+//
+//  Created by Damian Klimaszewski on 12.11.2014.
+//  Copyright (c) 2014 Michał Czwarnowski. All rights reserved.
+//
+
+#import "StatsViewController.h"
+
+@interface StatsViewController ()
+
+@end
+
+@implementation StatsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
